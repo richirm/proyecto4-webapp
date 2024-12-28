27e2393b61f868b02aabdea45bfe719e94db4de0
Vanessa Chipana <vchipani@emeal.nttdata.com>
2024-12-27 22:22:13 -0500

    feat: se agrega funciones de lógicas a los botones

---
+++ vanessa/botones/botones.js
@@ -0,0 +1,14 @@
+function cocinarProducto() {
+    confirm('¿Está seguro que quiere cocinar?');
+    console.log('cocina hecha!!');
+}
+
+function jugarPlay() {
+    confirm('¿Está seguro que desea jugar play?');
+    console.log('Empiece a jugar!!');
+}
+
+function volarAvion() {
+    confirm('¿Está seguro que desea volar avión?');
+    console.log('Empiece a volar el avión!!');
+}
