40d33a3fa8188bc8e764006b8808672070d69dd8
rarosales <rarosales@indracompany.com>
2025-04-11 21:00:18 -0500

    implelemta pantalla para pruebas javascript - funciones

---
+++ ricardo/javascript/javascript.js
@@ -0,0 +1,24 @@
+
+// para poder ejecutarla usando su nombre desde eventos
+// organizar o agrupar el codigo que queremos ejecutar
+// para poder diferenciarla de otras funciones
+function executarFuncion1() {
+    console.log(1111);
+    console.log(2222);
+}
+
+// Crear una funcion que haga log de la palabra "nadar en el mar"
+function nadarEnMar() {
+    console.log('nadar en el mar');
+}
+
+// Crear una funcion que haga log de la palabra "jugar con el perro"
+function jugarPerro() {
+    console.log('Jugar con el perro');
+}
+
+function nombrarPerro(nombrePerro, apellidoPerro, edadPerro) {
+    console.log('Nombre: ' + nombrePerro, apellidoPerro, edadPerro);
+}
+
+// Crear una funcion que haga log del nombre deel gato y que reciba el nombre en el parametro
