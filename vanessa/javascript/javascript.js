ff9c8c4a79a5fc24888e8bc7f469cd11fe690a46
vanesa chipana isidro <vchipanais16@gmail.com>
2025-05-13 20:59:49 -0500

    feat: Creación de componente en javascript

---
+++ vanessa/javascript/javascript.js
@@ -0,0 +1,258 @@
+function executarFuncion1() {
+  console.log(11111);
+  console.log(22222);
+}
+
+function nadarEnMar() {
+  console.log('Nadar en el mar');
+}
+
+function saltarGato() {
+  console.log('Salta gato');
+}
+
+function nombrarPerro(nombrePerro) {
+  console.log('Nombre: ' + nombrePerro);
+}
+
+function nombrarGato(nombreGato) {
+  console.log('Nombre: ' + nombreGato);
+}
+
+function sumarEdades() {
+  const edadPepito = 20;
+  const edadJuanito = 30;
+  
+  const resultado = edadPepito + edadJuanito;
+  console.log(resultado);
+}
+
+function sumarCostos() {
+  const costoPerro = 80;
+  const costoGato = 50;
+  
+  const resultado = costoPerro + costoGato;
+  console.log(resultado);
+}
+
+function calcularGanancia() {
+  const valorVenta = 250;
+  const costoInsumos = 70;
+  
+  const resultado = valorVenta - costoInsumos;
+  console.log(resultado);
+}
+
+function costoTotalPolloBrasa() {
+  const cantidadPollo = 100;
+  const precioPollo = 9;
+  
+  const resultado = cantidadPollo * precioPollo;
+  console.log(resultado);
+}
+
+
+function calcularNombreCompleto() {
+  const nombrePersona = 'Pepito';
+  const primerApellido = 'Gonzales';
+  const segundoApellido = 'Camacho';
+
+  const resultado1 = nombrePersona + ' ' + primerApellido + ' ' + segundoApellido; // concatenacion
+  const resultado2 = `${nombrePersona} ${primerApellido} ${segundoApellido}`; // string templates
+  const resultado3 = ''.concat(nombrePersona, ' ', primerApellido, ' ', segundoApellido); // operador concat
+
+  console.log('operador +:', resultado1);
+  console.log('string templates:', resultado2);
+  console.log('operador concat:', resultado3);
+}
+
+function calcularDireccionCompleta() {
+  const nombreAvenida = 'chinchaysuyo';
+  const distrito = 'Independencia';
+  const provincia = 'Lima';
+
+  const resultado1 = nombreAvenida + ' ' + distrito + ' ' + provincia; // concatenacion
+  const resultado2 = `${nombreAvenida} ${distrito} ${provincia}`; // string templates
+  const resultado3 = ''.concat(nombreAvenida, ' ', distrito, ' ', provincia); // operador concat
+
+  console.log('operador +:', resultado1);
+  console.log('string templates:', resultado2);
+  console.log('operador concat:', resultado3);
+}
+
+// Calcular la direccion completa: nombre avenida, distrito, provincia
+
+function calcularLongitudNombre() {
+  const nombreCompleto = 'Abdón Chipana Allauca';
+  const resultado = nombreCompleto.length;
+  console.log(resultado);
+}
+
+// Mayuscula y Minuscula
+function convertirNombreMayusculaMinuscula() {
+  const nombreCompleto = 'Pepito Gonzales Camacho';
+  const resultado1 = nombreCompleto.toUpperCase();
+  const resultado2 = nombreCompleto.toLowerCase();
+  console.log(resultado1);
+  console.log(resultado2);
+}
+
+// convertir a M/m la direccion
+function convertirDirreccionCompleta() {
+  const nombreCompleto = 'Pacaritambo 193 tahuantisuyo';
+  const resultado1 = nombreCompleto.toUpperCase();
+  const resultado2 = nombreCompleto.toLowerCase();
+  console.log(resultado1);
+  console.log(resultado2);
+}
+
+// Validar comienzo o fin de string
+function validarInicioFinNombre() {
+  const nombreCompleto = 'Pepito Gonzales Camacho';
+  const resultado1 = nombreCompleto.startsWith('Pepito');
+  const resultado2 = nombreCompleto.endsWith('Camacho1');
+  console.log(resultado1, resultado2);
+}
+
+// Validar comienzo o fin de string
+function validarDireccion() {
+  const nombreCompleto = 'jr pacaritambo 193';
+  const resultado1 = nombreCompleto.startsWith('j');
+  const resultado2 = nombreCompleto.endsWith('13');
+  console.log(resultado1, resultado2);
+}
+
+// Validar la direccion si empieza con '' o termina con ''
+function mostrarNombreCorrecto() {
+  const nombreCompleto = 'OTRO:Pepito Gonzales CamachoOTRO:';
+  const resultado = nombreCompleto.replaceAll('OTRO:', '');
+  console.log(resultado);
+}
+// mostrar la direccion correcta
+function obtenerApellidos() {
+  const nombreCompleto = 'Pepito Gonzales Camacho';
+  const resultado = nombreCompleto.substring(7);
+  console.log(resultado);
+}
+// mostrar una parte de la direccion
+function mostrarNombrePrimeraLetraMayuscula() {
+  const nombreCompleto = 'pepito Gonzales Camacho';
+  const resultado = nombreCompleto.charAt(0).toUpperCase() + nombreCompleto.substring(1);
+  console.log(resultado);
+}
+// mostrar la dirección capitalizada
+// Booleanos
+function validarSalirCalle() {
+  const estaLloviendo = false;
+  const tengoDinero = true;
+  const salgoCalle = !estaLloviendo && tengoDinero;
+  console.log(salgoCalle);
+}
+function validarSalirCalle() {
+  const estaLloviendo = false;
+  const tengoDinero = true;
+  const llamoMama = true;
+  const salgoCalle = !estaLloviendo && tengoDinero && !llamoMama;
+  console.log(salgoCalle);
+  // true && true => true
+  // false && true => false
+  // true && false => false
+  // false && false => false
+}
+function comprarSalchipapa() {
+  const daniPropina = true;
+  const alePropina = false;
+  const saleMagaly = true;
+  const comproSalchipapa = (daniPropina || alePropina) && saleMagaly;
+  console.log(comproSalchipapa);
+  // true || true => true
+  // true || false => true
+  // false || true => true
+  // false || false => false
+}
+function comprarMuñecos() {
+  const ventaMuñecas = true;
+  const saleVentas = false;
+  const comproMuñecas = (ventaMuñecas) && saleVentas;
+  console.log(comproMuñecas);
+  // true || true => true
+  // true || false => true
+  // false || true => true
+  // false || false => false
+}
+function obtenerAlumno() {
+  // clase Alumno
+  const alumno1 = {
+      nombre: 'ric',
+      primerApellido: 'ros',
+      segundoApellido: 'mald',
+      edad: 35,
+      fechaNacimiento: '01/11/1989',
+      gastaCine: true,
+  };
+  const alumno2 = {
+      nombre: 'emmir',
+      primerApellido: 'ros',
+      segundoApellido: 'chip',
+      edad: 0,
+      fechaNacimiento: '06/08/2024',
+      gastaCine: false,
+  };
+  console.log(alumno1);
+  console.log(alumno2);
+
+      // Acceder a un atributo
+      console.log(alumno1.edad); // acceso estatico al atributo
+      console.log(alumno1['edad']); // acceso dinamico al atributo
+      // Usar un atributo
+      const nombreCompleto1 = `${alumno1.nombre} ${alumno1.primerApellido} ${alumno1.segundoApellido}`;
+      console.log(nombreCompleto1);
+      // Modificar atributo
+      alumno1.primerApellido = 'rosales';
+      console.log(alumno1);
+      // Eliminar atributo
+      delete alumno1.otro;
+      console.log(alumno1);
+      // Copiar un objeto (dependiente)
+      const alumno3 = alumno1;
+      console.log(alumno3);
+      alumno1.segundoApellido = 'maldonado';
+      console.log(alumno3);
+      // Copiar un objeto (independiente)
+      const alumno4 = {...alumno1}; // spread operator
+      console.log(alumno4);
+      alumno1.nombre = 'ricardo';
+      console.log(alumno4);
+      console.log(alumno3);
+}
+function obtenerMenu() {
+  // menu
+  const menu1 = {
+      comida: 'tallarines',
+      primerMenu: 'pollo',
+      segundoMenu: 'carne',
+      precio: 15,
+      aprobado: true,
+  };
+  const menu2 = {
+    comida: 'estofado',
+    primerMenu: 'pollo',
+    segundoMenu: 'carne',
+    precio: 12,
+    aprobado: false,
+  };
+  console.log(menu1);
+  console.log(menu2);
+
+  console.log(menu1.precio); // acceso estatico al atributo
+  console.log(menu1['precio']); // acceso dinamico al atributo
+  const menuPrecio1 = `${menu1.comida} ${menu1.primerMenu} ${menu1.segundoMenu}`;
+  console.log(menuPrecio1);
+
+   // Modificar atributo
+   menu1.primerMenu = 'arroz';
+   console.log(menu1);
+   // Eliminar atributo
+   delete menu1.otro;
+   console.log(menu1);
+}
